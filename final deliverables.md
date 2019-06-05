df1e483dce0faa6d7d54d4bdde4786f64cf5e717
Shiva Sharma <shs101@ucsd.edu>
2019-06-05 01:31:25 -0700

    Comments added, milestone updated

--- final deliverables.md
+++ final deliverables.md
@@ -4,15 +4,28 @@
 Jen-Jun Yen: Ideation, Protoype, Adding functions into the News page,
 			 Modify the layout of the News page, Write-ups
 			 
-Shiva Sharma: 
+Shiva Sharma: Setting up backend with Firebase, implementing dynamic population of vaccine records and timeline with Firebase data, making json file using CDC data, UI design, milestone writeups.
 
 
 ## Source Code File:
-- index.html
+- index.html: This file implements functionality for the My Vaccines page of the web app.
+The file sets up two tables on the webpage, with Pending and Completed Vaccines, and 
+these tables are updated in real time as a user marks a vaccine as completed or still 
+pending. To populate and update tables, calls to the Firebase database are used. 
+Additionally, the user can view a list of all vaccines as well as search the database
+for some extra information such as diseases prevented by the vaccine and the symptoms.
+The file sets up a timeline on the webpage for the user to see what age ranges are 
+appropriate to get each vaccine administered.
 
-- news.html
+- news.html: This file implements the functionality for searching up news articles.
+The page calls the NEWS API. The page of news articles is initally populated with 
+articles related to the keyword "vaccine." The user can then make a search and it 
+will be appended with the keyword "vaccine." The results are returned in the form 
+of cards with article title, a few lines of the article, and an image.
 
-- sever.js
+- server.js: Starts up node server using express. The web server listens for interactions on static files, which are the index.html and news.html pages.
+
+- database.json: Contains information about each vaccine from the CDC website, including symptoms, disease information, and age ranges in a json format. The file is imported into Firebase.
 
 ## Slide
 
--- server.js
+++ server.js
@@ -1,39 +1,11 @@
+// Start up node server using express. 
+// The web server listens for interactions on static files, 
+// which are the index.html and news.html pages.
+
 const express = require('express');
 const app = express();
 
 app.use(express.static('static_files'));
-
-/*
-const vaccineDB = {
-  "Hepatitis B": {"starting age": "birth", "ending age": "18 months", "number of doses": "3"},
-  "Rotavirus": {"starting age": "2 months", "ending age": "6 months", "number of doses": "2"},
-  "Diptheria": {"starting age": "2 months", "ending age": "6 years", "number of doses": "4"},
-  "Tetanus": {"starting age": "2 months", "ending age": "6 years", "number of doses": "4"},
-  "Haemophilys influenzae type b": {"starting age": "2 months", "ending age": "15 months", "number of doses": "3"},
-  "Pneumococcal conjugate": {"starting age": "2 months", "ending age": "15 months", "number of doses": "4"},
-  "Inactivated poliovirus": {"starting age": "2 months", "ending age": "6 years", "number of doses": "3"},
-//  "Influenza": {"starting age": "6 months", "ending age": "", "number of doses": "1"},
-  "Varicella": {"starting age": "12 months", "ending age": "6 years", "number of doses": "2"},
-  "Hepatitis A": {"starting age": "12 months", "ending age": "6 years", "number of doses": "2"},
-  "Measles": {"starting age": "12 months", "ending age": "6 years", "number of doses": "2"}
-
-};
-*/
-
-
-// GET a list of all vaccines
-/*app.get('/allVaccines', (req, res) => {
-  database.ref
-  const allVaccines = Object.keys(vaccineDB); // returns a list of object keys
-  console.log('all vaccines is:', allVaccines);
-  res.send(allVaccines);
-});
-
-app.get('/hideAllVaccines', (req, res) => {
-  console.log('hiding vaccine list');
-  res.send({});
-}); 
-*/
 
 // start the server at URL: http://localhost:3000/
 app.listen(3000, () => {
